6ebfc3c76ebbe674f47e1cba40c92ca6aa21c3bd
FANT0M100 <giorgie103188@gmail.com>
2023-07-20 14:04:03 +0400

    Post file css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Post from "./components/Post";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <main>
+      <Post author="Giorgi" body="Hello World" />
+      <Post author="Anna" body="Hawe good day" />
+    </main>
+  );
 }
 
 export default App;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,13 @@
 import React from "react";
+import classes from "./Post.module.css";
 
-const Post = () => {
-  return <div></div>;
+const Post = ({ author, body }) => {
+  return (
+    <div className={classes.post}>
+      <p className={classes.author}>{author}</p>
+      <p className={classes.text}>{body}</p>
+    </div>
+  );
 };
 
 export default Post;
